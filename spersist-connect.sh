68cc9f63d2d9bb2664d12cfb0d70dad3c2ec2e74
dmoracze <dmoracze@gmail.com>
2019-09-03 16:03:13 -0400

    put final shh command in if statement

--- spersist-connect.sh
+++ spersist-connect.sh
@@ -2,19 +2,23 @@
 
 # check if .spersist exists on remote server
 ssh ${USER}@biowulf.nih.gov "test -e ~/.spersist"
+
+# download remote .spersist and connect
 if [ $? -eq 0 ]; then
 	echo
 	echo "Updating .spersist..."
 	echo
 	scp -q ${USER}@biowulf.nih.gov:~/.spersist ~
+	echo
+	echo "Connecting to session..."
+	echo
 	source ~/.spersist
+	# connect to spersist compute node
+	ssh -Y -t -L ${PORT1}:localhost:${PORT1} -L ${PORT_VNC}:localhost:${PORT_VNC} biowulf.nih.gov ssh -Y -t ${SLURMD_NODENAME}
 else
 	echo
 	echo "ERROR: Session information not found in biowulf.nih.gov:~/.spersist"
-	echo "Please run 'spersist-store.sh' within session to record session information"
+	echo "Please run 'spersist-store.sh' within spersist session to record session information"
 	echo
 	exit 1
 fi
-
-# connect to spersist compute node
-ssh -Y -t -L ${PORT1}:localhost:${PORT1} -L ${PORT_VNC}:localhost:${PORT_VNC} biowulf.nih.gov ssh -Y -t ${SLURMD_NODENAME}
